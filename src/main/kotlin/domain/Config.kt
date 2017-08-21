08a9f6f3dbd18df3f328a112a4b9a33d74ab791e
javacook <javacook@gmx.de>
2017-08-21 23:58:28 +0200

    1000 x 800

--- src/main/kotlin/domain/Config.kt
+++ src/main/kotlin/domain/Config.kt
@@ -3,5 +3,6 @@ package domain
 object Config {
     val noQuestions = 10
     val noAnswers = 4
-    val windowSize = Pair(500.0, 550.0)
+    val windowSize = Pair(1000.0, 800.0)
+//    val windowSize = Pair(500, 550.0)
 }
